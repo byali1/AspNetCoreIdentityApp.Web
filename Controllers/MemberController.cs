691bb170f3b108960bdfd134ea4b3f7b6fbffb4b
byali <ali_ilgin@outlook.com>
2024-03-10 21:55:54 +0300

    SignOut action

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIdentityApp.Web.Controllers
+{
+    public class MemberController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public MemberController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult LogOut()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        public IActionResult UserSettings()
+        {
+            return View();
+        }
+    }
+}
