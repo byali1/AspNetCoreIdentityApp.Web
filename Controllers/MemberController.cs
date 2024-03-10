2e59351460d3ba6143c62bfd239e8c3367b032f2
byali <ali_ilgin@outlook.com>
2024-03-10 21:57:02 +0300

    SignOut

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -13,9 +13,9 @@ namespace AspNetCoreIdentityApp.Web.Controllers
             _signInManager = signInManager;
         }
 
-        public IActionResult LogOut()
+        public async Task<IActionResult> LogOut()
         {
-            _signInManager.SignOutAsync();
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
 
